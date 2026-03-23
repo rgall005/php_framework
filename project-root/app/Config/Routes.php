f2ce7c9c3850d1a9df496a3325831ed88c702363
rgall005 <rgall005@gmail.com>
2026-03-23 13:50:02 -0500

    Delete project-root directory

--- project-root/app/Config/Routes.php
+++
@@ -1,4 +0,0 @@
-$routes->get('users', 'UserController::index');
-$routes->post('users/store', 'UserController::store');
-$routes->post('users/update/(:num)', 'UserController::update/$1');
-$routes->get('users/delete/(:num)', 'UserController::delete/$1');
--- project-root/app/Models/UserModel.php
+++
@@ -1,9 +0,0 @@
-<?php 
-namespace App\Models;
-use CodeIgniter\Model;
-
-class UserModel extends Model {
-    protected $table = 'users';
-    protected $primaryKey = 'id';
-    protected $allowedFields = ['name', 'email']; // Fields allowed for CRUD
-}
